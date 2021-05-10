d35120f697396b5b71a28601b81324df23e02d26
BachinDanila <nikto985@gmail.com>
2021-05-10 19:09:57 +0300

    first commit

---
+++ lex.cpp
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <string.h>
+#include "lex.hpp"
+
+struct LexTripl{
+  const char *str;
+  int str_len;
+  LexType type;
+};
+
+int isdigit(char chr)
+{
+  return chr >= '0' && chr <= '9';
+}
+
+int isletter(char chr)
+{
+  return (chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z');
+}
+
+int isendlex(char chr)
+{
+  return chr==' ' || chr=='\n' || chr=='\t'; 
+}
+
+int isoperator(char chr)
+{
+  return strchr("+-*/%<>=:,", chr) != NULL;
+}
+
+Lexem::Lexem(const char *str, LexType t, int line_)
+{
+	str_lex = new char[strlen(str)+1];
+	strcpy(str_lex, str);
+	type = t;
+	line = line_;
+}
+
+Lexem::~Lexem()
+{
+	delete[] str_lex;
+}
+
+LexType Lexem::getType()
+{
+	return type;
+}
+
+char *Lexem::getStr()
+{
+	return str_lex;
+}
+
+int Lexem::getLine()
+{
+	return line;
+}
+
+void Scanner::saveChr()
+{
+  *buf_top = chr; 
+  buf_top++;
+}
+
+Lexem *Scanner::next(char chr_)
+{ 
+  //Ignore new data if error occured
+  if(state != Err)
+    chr = chr_;
+  run();  
+  return lex;
+}
+
+void Scanner::run()
+{
+	switch(state){
+    case H:
+      analyze_H();
+      break;
+    case N:
+      analyze_N();
+      break;
+    case I:
+      analyze_I();
+      break;
+    case K:
+      analyze_K();
+      break;
+    case A:
+      analyze_A();
+      break;
+    case S:
+      analyze_S();
+      break;
+    case Err:
+      fprintf(stderr, "Unexpected '%c' at line %d\n", chr, line);
+      break;
+  }
+}
+
+void Scanner::setState(State state_)
+{
+    state = state_;
+    lex = NULL;
+    saveChr();
+}
+
+void Scanner::endLex(LexType type)
+{
+  state = H;
+  *buf_top = '\0';
+  lex = new Lexem(buf, type, line);
+	if(chr == '\n') 
+    line++;
+  memset(buf, '\0', MAX_BUFSIZE);
+  buf_top = buf;
+}
+
+LexType Scanner::bufKeywordType()
+{
+  const LexTripl table[11] =
+  { {"if",    2, Lif},    {"do",    2, Ldo},
+    {"then",  4, Lthen},  {"sell",  4, Lsell},
+    {"goto",  4, Lgoto},  {"prod",  4, Lprod},
+    {"print", 5, Lprint}, {"build", 5, Lbuild},
+    {"while", 5, Lwhile}, {"turn",  4, Lturn},
+    {"buy", 3, Lbuy} };
+  for(LexTripl t : table){
+    if(memcmp(t.str, buf, t.str_len) == 0)
+      return t.type;
+  }
+  return Lerr;
+}
+
+LexType Scanner::bufOperatorType()
+{
+  const LexTripl table[17] =
+  { {"+", 1, Lplus},  {">", 1, GT},
+    {"-", 1, Lminus}, {"=", 1, EQ},
+    {"*", 1, Lmult},  {"<", 1, LT},
+    {"/", 1, Ldiv},   {"&", 1, Land},
+    {"%", 1, Lmod},   {"|", 1, Lor},
+    {";", 1, Lsemi},  {",", 1, Lcomm},
+    {"(", 1, Llbrace},{"[", 1, Llsqbr},
+    {")", 1, Lrbrace},{"]", 1, Lrsqbr},
+    {"!", 1, Lnot} };
+  for(LexTripl t : table){
+    if(memcmp(t.str, buf, t.str_len) == 0)
+      return t.type;
+  }
+  return Lerr;
+}
+
+Scanner::Scanner()
+{
+	state = H;
+	line = 1;
+  buf_top = buf;
+}
+
+void Scanner::analyze_H()
+{
+  if(isendlex(chr)){
+    lex = NULL;
+    return;
+  } 
+  if(isdigit(chr)){
+    setState(N);
+    return;
+  } 
+  if(strchr("?@$", chr)){
+    setState(I);
+    return;
+  } 
+  if(isletter(chr)){
+    setState(K);
+    return;
+  } 
+  if(chr == ':'){
+    setState(A);
+    return;
+  } 
+  if(chr == '"'){
+    setState(S);
+    return;
+  }
+  if(strchr("+-*/%<>=,", chr)){ 
+    saveChr();
+    LexType type = bufOperatorType();
+    if(type != Lerr){
+      endLex(type);
+      return;
+    }
+  }
+  state = Err;
+}
+
+void Scanner::analyze_N()
+{
+  if(isendlex(chr) || isoperator(chr)){
+    endLex(Lnum);
+    return;
+  } 
+  if(isdigit(chr)){
+    setState(N);
+    return;
+  } 
+  state = Err;
+}
+
+void Scanner::analyze_I()
+{
+  if(isendlex(chr) || isoperator(chr)){
+    endLex(Lid);
+    return;
+  }
+  if(isletter(chr) || isdigit(chr)){
+    setState(I);
+    return;
+  }
+  state = Err;
+}
+
+void Scanner::analyze_K()
+{
+  if(isendlex(chr) || isoperator(chr)){
+    LexType type = bufKeywordType();
+    if(type != Lerr){
+      endLex(type);
+      return;
+    }
+  }
+  if(isletter(chr)){
+    setState(K);
+    return;
+  }
+  state = Err;
+}
+
+void Scanner::analyze_A()
+{
+	if(chr == '='){
+    saveChr();
+    endLex(Lassign);
+    return;
+	}
+  state = Err;
+}
+
+void Scanner::analyze_S()
+{
+	if(chr == '"'){
+    saveChr();
+    endLex(Lstring);
+    return;
+	}
+  setState(S);
+}
+
+std::ostream &operator<<(std::ostream &os, Lexem &lex)
+{
+      os << "Lexem: " << lex.getStr()   << 
+            " Type: " << lex.getType()  << 
+            " Line: " << lex.getLine();
+      return os;
+}
+
+int main(int argc, char *argv[])
+{
+	Lexem *lex;
+	char c;
+	FILE *file;
+	Scanner scan;
+
+	file = fopen(argv[1], "r");
+  if(!file)
+    fprintf(stderr, "Cannot open file\n");
+	while((c = fgetc(file)) != EOF){
+    lex = scan.next(c);
+    if(lex){
+      if(isoperator(c)){
+        std::cout << *lex << '\n';
+        lex = scan.next(c);
+      }
+      std::cout << *lex << '\n'; 
+    }
+	}
+	return 0;
+}
---
+++ lex.hpp
@@ -0,0 +1,59 @@
+#ifndef LEX_HPP
+#define LEX_HPP
+
+#define MAX_BUFSIZE 128
+
+enum LexType
+{
+	Lplus, Lminus, Lmult, Ldiv, Lmod,
+	EQ, LT , GT,
+	Land, Lor, Lnot,
+	Lsemi, Lcomm, 
+  Llbrace, Lrbrace, Llsqbr, Lrsqbr,
+  Lnum, Lstring, Lid, Lassign,
+	Lif, Lthen, Lwhile, Ldo, Lgoto,
+	Lprint,
+	Lbuy, Lsell, Lprod, Lbuild, Lturn,
+  Lempty, Lerr //Lerr - no matching type was found 
+};
+
+class Lexem
+{
+	LexType type;
+	char    *str_lex;
+	int     line;
+public:
+	Lexem(const char *str = "\0", LexType t = Lempty, int line = 0);
+	~Lexem();
+	LexType getType();
+	char    *getStr();
+	int     getLine();
+};
+
+class Scanner
+{
+	enum State {H, N, I, K, A, S, Err} state;
+  char    chr;
+  int     line;             //Line of code(by order) we parsing
+	char    buf[MAX_BUFSIZE], *buf_top;
+	Lexem   *lex; //Current lexeme, equals NULL if not finished yet
+public:
+	Scanner();
+	Lexem *next(char chr); //Null if not finished
+private:
+  void run();
+  void saveChr();
+  void setState(State state);
+  void endLex(LexType type);
+	void analyze_H();
+	void analyze_N();
+	void analyze_I();
+	void analyze_K();
+	void analyze_A();
+	void analyze_S();
+	void analyze_D();
+  LexType bufKeywordType();
+  LexType bufOperatorType();
+};
+
+#endif
